8d2c1bacd5e618065554c71c036b1844d9efa802
reychino-heisenberg <reychino@gmail.com>
2016-04-17 10:25:57 -0500

    kptsConv script modified.

--- convergence/kpts_conv/kptsConv.sh
+++ convergence/kpts_conv/kptsConv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-kpts=( 452 802 1252 1802 2452 3202 5002 6052 7202 )
+# kpts=( 452 802 1252 1802 2452 3202 5002 6052 7202 )
+kpts=( 8452  9802 11252 12802 )
 
 for i in ${kpts[@]}; do
+    START=$(date)
     run_tiniba.sh -r run -k "$i" -N 0 -x 2 -C 6 -P 1382 -w -e -p -s
-    ssh cassandra "echo -e 'Calculations completed for $i kpts \n\nArray of ktps: \n${kpts[@]} ' | mail -s '$i kpts' reychino@gmail.com"
+    END=$(date)
+    ssh cassandra "echo -e 'Calculations completed for $i kpts\nStarted at $START \nFinished at $END \n\nArray of ktps: \n${kpts[@]} ' | mail -s '$i kpts' reychino@gmail.com"
 done
